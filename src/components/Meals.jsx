27feb1b9e06bb071cdf717b9e151eaf48a5191f5
Maria <109592079+MariaDucuSchiopea@users.noreply.github.com>
2022-09-07 17:01:57 +0000

    Version using Fetch (next will be using axios)

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,8 @@
-import {useGlobalContext} from '../context'
+import { useGlobalContext } from '../context'
 
 const Meals = () => {
-  const context = useGlobalContext
+  const context = useGlobalContext()
   return <h1>Meals component</h1>
 }
-  
+
 export default Meals;
